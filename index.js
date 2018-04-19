9dd80c6d3ce823928bb4322632652957acb2acc2
damith <damithasanka@gmail.com>
2018-04-19 12:09:35 +0530

    init checking

---
+++ index.js
@@ -0,0 +1,33 @@
+let restify = require('restify');
+let bunyan = require("bunyan");
+let config = require("./config");
+
+let respond = (req,res,next)=>{
+    res.send('hello '+req.params.name);
+    next();
+}
+
+const server = restify.createServer({
+    log:bunyan.createLogger({
+        name:'myapp'
+    })
+});
+server.get('/:name',respond);
+server.head('/hello/:name',respond);
+
+server.listen(config.port, () => {
+    // mongoose.Promise = global.Promise;
+    // mongoose.connect(config.db.uri);
+
+    // const db = mongoose.connection;
+
+    // db.on('error', (err) => {
+    //     console.error(err);
+    //     process.exit(1);
+    // });
+    // db.once('open', () => {
+    //     routes(server);
+    //     console.log(`Server is listening on port ${config.port}`);
+    // });
+    console.log(`Server is listening on port ${config.port}`);
+});
